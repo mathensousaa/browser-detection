8f24be1831c11a500462b2dd48d7ccd7ae228c0b
mathensousaa <mhenrique.sousa43@gmail.com>
2022-08-27 13:59:12 -0300

    Recebendo e imprimindo navegador em uso

---
+++ scripts.js
@@ -0,0 +1,28 @@
+var browserName = (function(agent) {           
+    switch (true) {
+        case agent.indexOf("edge") > -1:
+            return "MS Edge";
+
+        case agent.indexOf("edg/") > -1:
+            return "Edge (chromium based)";
+
+        case agent.indexOf("opr") > -1 && !!window.opr:
+            return "Opera";
+
+        case agent.indexOf("chrome") > -1 && !!window.chrome:
+            return "Chrome";
+
+        case agent.indexOf("trident") > -1:
+            return "MS IE";
+
+        case agent.indexOf("firefox") > -1:
+            return "Mozzila Firefox";
+
+        case agent.indexOf("Safari") > -1:
+            return "Safari";
+
+        default: return "outro";
+    }
+})(window.navigator.userAgent.toLowerCase());
+
+document.querySelector("h2").innerText="Você esta usando o navegador " + browserName; 
